0e1f0ecfe6ff6cf8be6285cf44e8f9ea3e76d85e
Philipp Zabel <philipp.zabel@gmail.com>
2016-07-06 21:18:24 +0200

    Make OpenCV dependency configurable
    OpenCV is currently only used to get an initial pose estimate for the Rift DK2.
    Make it optional.

--- src/opencv.h
+++ src/opencv.h
@@ -8,9 +8,28 @@
 
 #include "math.h"
 
+#if HAVE_OPENCV
 void estimate_initial_pose(struct blob *blobs, int num_blobs,
 			   vec3 *leds, int num_leds,
 			   dmat3 *camera_matrix, double dist_coeffs[5],
 			   dquat *rot, dvec3 *trans, bool use_extrinsic_guess);
+#else
+static inline
+void estimate_initial_pose(struct blob *blobs, int num_blobs,
+			   vec3 *leds, int num_leds,
+			   dmat3 *camera_matrix, double dist_coeffs[5],
+			   dquat *rot, dvec3 *trans, bool use_extrinsic_guess)
+{
+	(void)blobs;
+	(void)num_blobs;
+	(void)leds;
+	(void)num_leds;
+	(void)camera_matrix;
+	(void)dist_coeffs;
+	(void)rot;
+	(void)trans;
+	(void)use_extrinsic_guess;
+}
+#endif
 
 #endif /* __OPENCV_H__ */
