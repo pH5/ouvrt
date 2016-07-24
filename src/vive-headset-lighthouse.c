8f1d3ad1d3bfc63d7b9a96793c8ff12ced7583d1
Philipp Zabel <philipp.zabel@gmail.com>
2016-07-24 17:43:58 +0200

    Vive: add rudimentary Lighthouse RX sync pulse detection
    Sync pulses are longer than the sensor hits caused by sweeps,
    and many sensors register them at the same time. The sync pulse
    length encodes three bits of information: whether the sync pulse
    is associated with the horizontal or vertical rotor, whether the
    base station will skip the following sweep, and an OOTX bit that
    is used to transfer the OOTX frame over time.

--- src/vive-headset-lighthouse.c
+++ src/vive-headset-lighthouse.c
@@ -18,13 +18,128 @@
 #include "vive-hid-reports.h"
 #include "device.h"
 
+enum pulse_mode {
+	SYNC,
+	SWEEP
+};
+
+struct lighthouse_pulse {
+	uint32_t timestamp;
+	uint16_t duration;
+};
+
 struct _OuvrtViveHeadsetLighthousePrivate {
 	gboolean base_visible;
+
+	enum pulse_mode mode;
+	uint32_t seen_by;
 	uint32_t last_timestamp;
+	uint16_t duration;
+	struct lighthouse_pulse last_sync;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE(OuvrtViveHeadsetLighthouse, ouvrt_vive_headset_lighthouse, \
 			   OUVRT_TYPE_DEVICE)
+
+struct lighthouse_sync_pulse {
+	uint16_t duration;
+	gboolean skip;
+	gboolean rotor;
+	gboolean data;
+};
+
+static struct lighthouse_sync_pulse pulse_table[8] = {
+	{ 3000, 0, 0, 0 },
+	{ 3500, 0, 1, 0 },
+	{ 4000, 0, 0, 1 },
+	{ 4500, 0, 1, 1 },
+	{ 5000, 1, 0, 0 },
+	{ 5500, 1, 1, 0 },
+	{ 6000, 1, 0, 1 },
+	{ 6500, 1, 1, 1 },
+};
+
+static void
+vive_headset_lighthouse_handle_sync_pulse(OuvrtViveHeadsetLighthouse *self,
+					  struct lighthouse_pulse *sync)
+{
+	OuvrtViveHeadsetLighthousePrivate *priv = self->priv;
+	struct lighthouse_sync_pulse *pulse;
+	int32_t dt;
+	int i;
+
+	if (!sync->duration)
+		return;
+
+	for (i = 0, pulse = pulse_table; i < 8; i++, pulse++) {
+		if (sync->duration > (pulse->duration - 250) &&
+		    sync->duration < (pulse->duration + 250)) {
+			break;
+		}
+	}
+	if (i == 8) {
+		g_print("%s: Unknown pulse length: %d\n", self->dev.name,
+			sync->duration);
+		return;
+	}
+
+	dt = sync->timestamp - priv->last_timestamp;
+
+	/* 48 MHz / 120 Hz = 400000 cycles per sync pulse */
+	if (dt > (400000 - 4000) && dt < (400000 + 4000)) {
+		/* Observing a single base station, channel A */
+	} else if (dt > (380000 - 4000) && dt < (380000 + 4000)) {
+		/* Observing two base stations, this is channel B */
+	} else if (dt > (20000 - 4000) && dt < (20000 + 4000)) {
+		/* Observing two base stations, this is channel C */
+	} else {
+		/* Irregular sync pulse */
+		if (priv->last_timestamp)
+			g_print("%s: Irregular sync pulse: %u -> %u (%+d)\n",
+				self->dev.name, priv->last_timestamp,
+				sync->timestamp, dt);
+	}
+
+	priv->last_timestamp = sync->timestamp;
+}
+
+static void
+vive_headset_lighthouse_handle_pulse(OuvrtViveHeadsetLighthouse *self,
+				     uint8_t id, uint16_t duration,
+				     uint32_t timestamp)
+{
+	OuvrtViveHeadsetLighthousePrivate *priv = self->priv;
+	int32_t dt;
+
+	dt = timestamp - priv->last_sync.timestamp;
+
+	if (priv->mode == SYNC && priv->seen_by && dt > priv->last_sync.duration) {
+		vive_headset_lighthouse_handle_sync_pulse(self, &priv->last_sync);
+		priv->seen_by = 0;
+	}
+
+	if (duration >= 2750) {
+		if (dt > priv->last_sync.duration || priv->last_sync.duration == 0) {
+			priv->seen_by = 1 << id;
+			priv->last_sync.timestamp = timestamp;
+			priv->last_sync.duration = duration;
+		} else {
+			priv->seen_by |= 1 << id;
+			if (timestamp < priv->last_sync.timestamp) {
+				priv->last_sync.duration += priv->last_sync.timestamp - timestamp;
+				priv->last_sync.timestamp = timestamp;
+			}
+			if (duration > priv->last_sync.duration)
+				priv->last_sync.duration = duration;
+			priv->last_sync.duration = duration;
+		}
+		priv->mode = SYNC;
+	} else {
+		if (priv->mode == SYNC && dt <= priv->last_sync.duration)
+			g_print("%s: error, expected sync\n", self->dev.name);
+		priv->mode = SWEEP;
+	}
+}
 
 /*
  * Decodes the periodic Lighthouse receiver message containing IR pulse
@@ -68,8 +183,8 @@ vive_headset_lighthouse_decode_pulse_report1(OuvrtViveHeadsetLighthouse *self,
 
 		duration = __le16_to_cpu(pulse->duration);
 
-		(void)duration;
-		(void)timestamp;
+		vive_headset_lighthouse_handle_pulse(self, sensor_id, duration,
+						     timestamp);
 	}
 }
 
@@ -107,8 +222,8 @@ vive_headset_lighthouse_decode_pulse_report2(OuvrtViveHeadsetLighthouse *self,
 
 		duration = __le16_to_cpu(pulse->duration);
 
-		(void)duration;
-		(void)timestamp;
+		vive_headset_lighthouse_handle_pulse(self, sensor_id, duration,
+						     timestamp);
 	}
 }
 
@@ -217,6 +332,12 @@ static void ouvrt_vive_headset_lighthouse_init(OuvrtViveHeadsetLighthouse *self)
 {
 	self->dev.type = DEVICE_TYPE_HMD;
 	self->priv = ouvrt_vive_headset_lighthouse_get_instance_private(self);
+
+	self->priv->mode = SWEEP;
+	self->priv->seen_by = 0;
+	self->priv->last_timestamp = 0;
+	self->priv->last_sync.timestamp = 0;
+	self->priv->last_sync.duration = 0;
 }
 
 /*
