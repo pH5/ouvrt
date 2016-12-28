ec9cbcedcee097e03d75b8f1c3e4443974678782
Philipp Zabel <philipp.zabel@gmail.com>
2016-12-28 14:44:54 +0100

    Rift DK2: fix esp570_setup_unknown_3 declaration
    This function returns an integer value.

--- src/esp570.h
+++ src/esp570.h
@@ -8,4 +8,4 @@
 int esp570_eeprom_read(int fd, uint16_t addr, uint8_t len, char *buf);
 int esp570_i2c_read(int fd, uint8_t addr, uint8_t reg, uint16_t *val);
 int esp570_i2c_write(int fd, uint8_t addr, uint8_t reg, uint16_t val);
-void esp570_setup_unknown_3(int fd);
+int esp570_setup_unknown_3(int fd);
