569ec3f78d2ca147931bd18f871739a6da51dbca
Philipp Zabel <philipp.zabel@gmail.com>
2016-12-29 20:11:09 +0100

    Vive: parse lighthouse sensor configuration
    Read the lighthouse sensor positions and normals into a tracking model
    structure.

--- src/json.c
+++ src/json.c
@@ -6,6 +6,7 @@
 #include <json-glib/json-glib.h>
 
 #include "json.h"
+#include "tracking-model.h"
 
 void json_object_get_vec3_member(JsonObject *object,
 				 const char *member_name,
@@ -26,4 +27,53 @@ void json_array_get_vec3_element(JsonArray *array, guint index,
 	out->x = json_array_get_double_element(element, 0);
 	out->y = json_array_get_double_element(element, 1);
 	out->z = json_array_get_double_element(element, 2);
+}
+
+void json_object_get_lighthouse_config_member(JsonObject *object,
+					      const gchar *member_name,
+					      struct tracking_model *model)
+{
+	JsonObject *config;
+	JsonArray *channel_map, *model_normals, *model_points;
+	gint64 num_channels, num_normals, num_points;
+	int i;
+
+	config = json_object_get_object_member(object, member_name);
+	if (!config)
+		return;
+
+	channel_map = json_object_get_array_member(config, "channelMap");
+	if (!channel_map)
+		return;
+
+	num_channels = json_array_get_length(channel_map);
+	for (i = 0; i < num_channels; i++) {
+		gint64 channel_id = json_array_get_int_element(channel_map, i);
+
+		if (channel_id != i)
+			return;
+	}
+
+	model_normals = json_object_get_array_member(config, "modelNormals");
+	if (!model_normals)
+		return;
+
+	num_normals = json_array_get_length(model_normals);
+	if (num_normals != num_channels)
+		return;
+
+	model_points = json_object_get_array_member(config, "modelPoints");
+	if (!model_points)
+		return;
+
+	num_points = json_array_get_length(model_points);
+	if (num_points != num_channels)
+		return;
+
+	tracking_model_init(model, num_points);
+
+	for (i = 0; i < num_points; i++) {
+		json_array_get_vec3_element(model_normals, i, &model->normals[i]);
+		json_array_get_vec3_element(model_points, i, &model->points[i]);
+	}
 }
--- src/json.h
+++ src/json.h
@@ -10,10 +10,16 @@
 
 #include "math.h"
 
+struct tracking_model;
+
 void json_object_get_vec3_member(JsonObject *object, const char *member,
 				 vec3 *out);
 
 void json_array_get_vec3_element(JsonArray *array, guint index,
 				 vec3 *out);
+
+void json_object_get_lighthouse_config_member(JsonObject *object,
+					      const gchar *member_name,
+					      struct tracking_model *model);
 
 #endif /* __JSON_H__ */
