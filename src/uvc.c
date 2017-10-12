27872995fc74219453d34a3bf948cf21ab2aedbc
Philipp Zabel <p.zabel@pengutronix.de>
2017-10-12 19:49:25 +0200

    Add libusb UVC code
    Add UVC controls via libusb and a structure for the probe/commit control.

---
+++ src/uvc.c
@@ -0,0 +1,77 @@
+/*
+ * UVC Controls
+ * Copyright 2017 Philipp Zabel
+ * SPDX-License-Identifier:	LGPL-2.0+
+ */
+#include <glib.h>
+#include <libusb.h>
+#include <stdint.h>
+
+#define SET_CUR			0x01
+#define GET_CUR			0x81
+#define GET_LEN			0x85
+#define TIMEOUT			1000
+
+#define VS_PROBE_CONTROL	1
+#define VS_COMMIT_CONTROL	2
+
+int uvc_set_cur(libusb_device_handle *dev, uint8_t interface, uint8_t entity,
+		uint8_t selector, void *data, uint16_t wLength)
+{
+	uint8_t bmRequestType = LIBUSB_ENDPOINT_OUT |
+		LIBUSB_REQUEST_TYPE_CLASS |
+		LIBUSB_RECIPIENT_INTERFACE;
+	uint8_t bRequest = SET_CUR;
+	uint16_t wValue = selector << 8;
+	uint16_t wIndex = entity << 8 | interface;
+	int ret;
+
+	ret = libusb_control_transfer(dev, bmRequestType, bRequest, wValue,
+				      wIndex, data, wLength, TIMEOUT);
+	if (ret < 0) {
+		g_print("UVC: Failed to transfer SET CUR %u %u %u: %d (%s)\n",
+			interface, entity, selector, ret, libusb_strerror(ret));
+	}
+	return ret;
+}
+
+int uvc_get_cur(libusb_device_handle *dev, uint8_t interface, uint8_t entity,
+		uint8_t selector, void *data, uint16_t wLength)
+{
+	uint8_t bmRequestType = LIBUSB_ENDPOINT_IN |
+				LIBUSB_REQUEST_TYPE_CLASS |
+				LIBUSB_RECIPIENT_INTERFACE;
+	uint8_t bRequest = GET_CUR;
+	uint16_t wValue = selector << 8;
+	uint16_t wIndex = entity << 8 | interface;
+	int ret;
+
+	ret = libusb_control_transfer(dev, bmRequestType, bRequest, wValue,
+				      wIndex, data, wLength, TIMEOUT);
+        if (ret < 0) {
+		g_print("UVC: Failed to transfer GET CUR %u %u %u: %d (%s)\n",
+			interface, entity, selector, ret, libusb_strerror(ret));
+	}
+	return ret;
+}
+
+int uvc_get_len(libusb_device_handle *dev, uint8_t interface, uint8_t entity,
+		uint8_t selector, uint16_t *wLength)
+{
+	uint8_t bmRequestType = LIBUSB_ENDPOINT_IN |
+				LIBUSB_REQUEST_TYPE_CLASS |
+				LIBUSB_RECIPIENT_INTERFACE;
+	uint8_t bRequest = GET_LEN;
+	uint16_t wValue = selector << 8;
+	uint16_t wIndex = entity << 8 | interface;
+	int ret;
+
+	ret = libusb_control_transfer(dev, bmRequestType, bRequest, wValue,
+				      wIndex, (void *)wLength, sizeof(*wLength),
+				      TIMEOUT);
+	if (ret < 0) {
+		g_print("UVC: Failed to transfer GET LEN %u %u %u: %d (%s)\n",
+			interface, entity, selector, ret, libusb_strerror(ret));
+	}
+	return ret;
+}
---
+++ src/uvc.h
@@ -0,0 +1,52 @@
+/*
+ * UVC Controls
+ * Copyright 2017 Philipp Zabel
+ * SPDX-License-Identifier:	LGPL-2.0+
+ */
+#include <asm/byteorder.h>
+#include <libusb.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define VS_PROBE_CONTROL	1
+#define VS_COMMIT_CONTROL	2
+
+struct uvc_probe_commit_control {
+	__le16 bmHint;
+	__u8 bFormatIndex;
+	__u8 bFrameIndex;
+	__le32 dwFrameInterval;
+	__le16 wKeyFrameRate;
+	__le16 wPFrameRate;
+	__le16 wCompQuality;
+	__le16 wCompWindowSize;
+	__le16 wDelay;
+	__le32 dwMaxVideoFrameSize;
+	__le32 dwMaxPayloadTransferSize;
+	__le32 dwClockFrequency;
+	__u8 bmFramingInfo;
+	__u8 bPreferedVersion;
+	__u8 bMinVersion;
+	__u8 bMaxVersion;
+} __attribute__((packed));
+
+struct uvc_payload_header {
+	__u8 bHeaderLength;
+	__u8 bmHeaderInfo;
+	__le32 dwPresentationTime;
+	__u8 scrSourceClock[6];
+} __attribute__((packed));
+
+#define ASSERT_SIZE(report,size) \
+        _Static_assert((sizeof(struct report) == size), \
+                       "incorrect size: " #report)
+
+ASSERT_SIZE(uvc_probe_commit_control, 34);
+ASSERT_SIZE(uvc_payload_header, 12);
+
+int uvc_set_cur(libusb_device_handle *dev, uint8_t interface, uint8_t entity,
+		uint8_t selector, void *data, uint16_t wLength);
+int uvc_get_cur(libusb_device_handle *dev, uint8_t interface, uint8_t entity,
+		uint8_t selector, void *data, uint16_t wLength);
+int uvc_get_len(libusb_device_handle *dev, uint8_t interface, uint8_t entity,
+		uint8_t selector, uint16_t *wLength);
